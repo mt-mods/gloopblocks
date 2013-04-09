32e814f6845135f30294de7edbf2b8816f23400e
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2013-04-08 20:31:58 -0400

    Moved crafting recipes into a separate file.
    Added crafts for mossy stone brick.  Added an abm to transform stone brick into
    mossy stone brick in the presence of water.

---
+++ crafts.lua
@@ -0,0 +1,257 @@
+-- Various crafts
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "gloopblocks:rainbow_block",
+	recipe = {
+		"group:basecolor_red",
+		"group:excolor_orange",
+		"group:basecolor_yellow",
+		"group:basecolor_green",
+		"group:basecolor_blue",
+		"group:excolor_violet",
+		"default:stone",
+		"default:mese_crystal",
+	},
+})
+
+minetest.register_craft({
+	output = "default:nyancat",
+	recipe = {
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+	}
+})
+
+minetest.register_craft({
+	output = "default:nyancat_rainbow",
+	recipe = {
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+	}
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "gloopblocks:wet_cement",
+	recipe = {
+		"bucket:bucket_water",
+		"default:gravel",
+	},
+	replacements = {{'bucket:bucket_water', 'bucket:bucket_empty'},},
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "gloopblocks:cement",
+	recipe = "gloopblocks:wet_cement",
+	cooktime = 8
+})
+
+minetest.register_craft({
+	output = "default:gravel 2",
+	recipe = {
+		{"gloopblocks:cement"},
+	},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "gloopblocks:evil_stick",
+	recipe = {
+		"gloopores:kalite_lump",
+		"default:coal_lump",
+		"default:stick"
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:evil_block",
+	recipe = {
+		{"gloopblocks:evil_stick", "gloopblocks:evil_stick"},
+		{"gloopblocks:evil_stick", "gloopblocks:evil_stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:evil_stick 4",
+	recipe = {
+		{"gloopblocks:evil_block"}
+	}
+})
+
+minetest.register_craft({
+	output = "default:nyancat",
+	recipe = {
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+	}
+})
+
+minetest.register_craft({
+	output = "default:nyancat_rainbow",
+	recipe = {
+		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:pick_cement",
+	recipe = {
+		{"gloopblocks:cement", "gloopblocks:cement", "gloopblocks:cement"},
+		{"", "default:stick", ""},
+		{"", "default:stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:axe_cement",
+	recipe = {
+		{"gloopblocks:cement", "gloopblocks:cement"},
+		{"gloopblocks:cement", "default:stick"},
+		{"", "default:stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:axe_cement",
+	recipe = {
+		{"gloopblocks:cement", "gloopblocks:cement"},
+		{"default:stick", "gloopblocks:cement"},
+		{"default:stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:shovel_cement",
+	recipe = {
+		{"gloopblocks:cement"},
+		{"default:stick"},
+		{"default:stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:sword_cement",
+	recipe = {
+		{"gloopblocks:cement"},
+		{"gloopblocks:cement"},
+		{"default:stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:pick_evil",
+	recipe = {
+		{"gloopblocks:evil_block", "gloopblocks:evil_block", "gloopblocks:evil_block"},
+		{"", "gloopblocks:evil_stick", ""},
+		{"", "gloopblocks:evil_stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:axe_evil",
+	recipe = {
+		{"gloopblocks:evil_block", "gloopblocks:evil_block"},
+		{"gloopblocks:evil_block", "gloopblocks:evil_stick"},
+		{"", "gloopblocks:evil_stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:axe_evil",
+	recipe = {
+		{"gloopblocks:evil_block", "gloopblocks:evil_block"},
+		{"gloopblocks:evil_stick", "gloopblocks:evil_block"},
+		{"gloopblocks:evil_stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:shovel_evil",
+	recipe = {
+		{"gloopblocks:evil_block"},
+		{"gloopblocks:evil_stick"},
+		{"gloopblocks:evil_stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:sword_evil",
+	recipe = {
+		{"gloopblocks:evil_block"},
+		{"gloopblocks:evil_block"},
+		{"gloopblocks:evil_stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "default:wood",
+	recipe = {
+		 {"default:stick", "default:stick"},
+		 {"default:stick", "default:stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:pavement 5",
+	recipe = {
+		{"gloopblocks:basalt",    "gloopblocks:wet_cement","gloopblocks:basalt"},
+		{"gloopblocks:wet_cement","gloopblocks:basalt",    "gloopblocks:wet_cement"},
+		{"gloopblocks:basalt",    "gloopblocks:wet_cement","gloopblocks:basalt"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:pavement 5",
+	recipe = {
+		{"gloopblocks:wet_cement","gloopblocks:basalt",    "gloopblocks:wet_cement"},
+		{"gloopblocks:basalt",    "gloopblocks:wet_cement","gloopblocks:basalt"},
+		{"gloopblocks:wet_cement","gloopblocks:basalt",    "gloopblocks:wet_cement"},
+	}
+})
+
+minetest.register_craft({
+	output = "gloopblocks:oerkki_block 2",
+	recipe = {
+		{"default:iron_lump", "default:coal_lump", "default:iron_lump"},
+		{"default:coal_lump", "default:bookshelf", "default:coal_lump"},
+		{"default:iron_lump", "default:coal_lump", "default:iron_lump"},
+	},
+	replacements = { { "default:bookshelf", "default:book 3" } }
+})
+
+minetest.register_craft({
+	output = "gloopblocks:oerkki_block 2",
+	recipe = {
+		{"default:coal_lump", "default:iron_lump", "default:coal_lump"},
+		{"default:iron_lump", "default:bookshelf", "default:iron_lump"},
+		{"default:coal_lump", "default:iron_lump", "default:coal_lump"},
+	},
+	replacements = { { "default:bookshelf", "default:book 3" } }
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "gloopblocks:stone_brick_mossy",
+	recipe = {
+		"default:stonebrick",
+		"default:stonebrick",
+		"default:junglegrass",
+		"default:junglegrass"
+	}
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "gloopblocks:stone_brick_mossy",
+	recipe = {
+		"default:stonebrick",
+		"default:stonebrick",
+		"default:grass_1",
+		"default:grass_1",
+		"default:grass_1",
+		"default:grass_1"
+	}
+})
--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
 --[[
 GloopBlocks
 written by GloopMaster
-Version "Whatever you wanna call it"
 Licensed under WTFPL.
 
-Updates by VanessaE 2013-03-17.
+Updates by VanessaE.
+
+2013-04-08
 
 the register_gloopblocks_alias function is borrowed from stairsplus.
 
@@ -281,241 +282,7 @@ minetest.register_craftitem("gloopblocks:evil_stick", {
 	inventory_image = "gloopblocks_evil_stick.png",
 })
 
--- Crafts
-
-minetest.register_craft( {
-	type = "shapeless",
-	output = "gloopblocks:rainbow_block",
-	recipe = {
-		"group:basecolor_red",
-		"group:excolor_orange",
-		"group:basecolor_yellow",
-		"group:basecolor_green",
-		"group:basecolor_blue",
-		"group:excolor_violet",
-		"default:stone",
-		"default:mese_crystal",
-	},
-})
-
-minetest.register_craft({
-	output = "default:nyancat",
-	recipe = {
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-	}
-})
-
-minetest.register_craft({
-	output = "default:nyancat_rainbow",
-	recipe = {
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-	}
-})
-
-minetest.register_craft({
-	type = "shapeless",
-	output = "gloopblocks:wet_cement",
-	recipe = {
-		"bucket:bucket_water",
-		"default:gravel",
-	},
-	replacements = {{'bucket:bucket_water', 'bucket:bucket_empty'},},
-})
-
-minetest.register_craft({
-	type = "cooking",
-	output = "gloopblocks:cement",
-	recipe = "gloopblocks:wet_cement",
-	cooktime = 8
-})
-
-minetest.register_craft({
-	output = "default:gravel 2",
-	recipe = {
-		{"gloopblocks:cement"},
-	},
-})
-
-minetest.register_craft({
-	type = "shapeless",
-	output = "gloopblocks:evil_stick",
-	recipe = {
-		"gloopores:kalite_lump",
-		"default:coal_lump",
-		"default:stick"
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:evil_block",
-	recipe = {
-		{"gloopblocks:evil_stick", "gloopblocks:evil_stick"},
-		{"gloopblocks:evil_stick", "gloopblocks:evil_stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:evil_stick 4",
-	recipe = {
-		{"gloopblocks:evil_block"}
-	}
-})
-
-minetest.register_craft({
-	output = "default:nyancat",
-	recipe = {
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-	}
-})
-
-minetest.register_craft({
-	output = "default:nyancat_rainbow",
-	recipe = {
-		{"gloopblocks:rainbow_block", "gloopblocks:rainbow_block", "gloopblocks:rainbow_block"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:pick_cement",
-	recipe = {
-		{"gloopblocks:cement", "gloopblocks:cement", "gloopblocks:cement"},
-		{"", "default:stick", ""},
-		{"", "default:stick", ""},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:axe_cement",
-	recipe = {
-		{"gloopblocks:cement", "gloopblocks:cement"},
-		{"gloopblocks:cement", "default:stick"},
-		{"", "default:stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:axe_cement",
-	recipe = {
-		{"gloopblocks:cement", "gloopblocks:cement"},
-		{"default:stick", "gloopblocks:cement"},
-		{"default:stick", ""},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:shovel_cement",
-	recipe = {
-		{"gloopblocks:cement"},
-		{"default:stick"},
-		{"default:stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:sword_cement",
-	recipe = {
-		{"gloopblocks:cement"},
-		{"gloopblocks:cement"},
-		{"default:stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:pick_evil",
-	recipe = {
-		{"gloopblocks:evil_block", "gloopblocks:evil_block", "gloopblocks:evil_block"},
-		{"", "gloopblocks:evil_stick", ""},
-		{"", "gloopblocks:evil_stick", ""},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:axe_evil",
-	recipe = {
-		{"gloopblocks:evil_block", "gloopblocks:evil_block"},
-		{"gloopblocks:evil_block", "gloopblocks:evil_stick"},
-		{"", "gloopblocks:evil_stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:axe_evil",
-	recipe = {
-		{"gloopblocks:evil_block", "gloopblocks:evil_block"},
-		{"gloopblocks:evil_stick", "gloopblocks:evil_block"},
-		{"gloopblocks:evil_stick", ""},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:shovel_evil",
-	recipe = {
-		{"gloopblocks:evil_block"},
-		{"gloopblocks:evil_stick"},
-		{"gloopblocks:evil_stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:sword_evil",
-	recipe = {
-		{"gloopblocks:evil_block"},
-		{"gloopblocks:evil_block"},
-		{"gloopblocks:evil_stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "default:wood",
-	recipe = {
-		 {"default:stick", "default:stick"},
-		 {"default:stick", "default:stick"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:pavement 5",
-	recipe = {
-		{"gloopblocks:basalt",    "gloopblocks:wet_cement","gloopblocks:basalt"},
-		{"gloopblocks:wet_cement","gloopblocks:basalt",    "gloopblocks:wet_cement"},
-		{"gloopblocks:basalt",    "gloopblocks:wet_cement","gloopblocks:basalt"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:pavement 5",
-	recipe = {
-		{"gloopblocks:wet_cement","gloopblocks:basalt",    "gloopblocks:wet_cement"},
-		{"gloopblocks:basalt",    "gloopblocks:wet_cement","gloopblocks:basalt"},
-		{"gloopblocks:wet_cement","gloopblocks:basalt",    "gloopblocks:wet_cement"},
-	}
-})
-
-minetest.register_craft({
-	output = "gloopblocks:oerkki_block 2",
-	recipe = {
-		{"default:iron_lump", "default:coal_lump", "default:iron_lump"},
-		{"default:coal_lump", "default:bookshelf", "default:coal_lump"},
-		{"default:iron_lump", "default:coal_lump", "default:iron_lump"},
-	},
-	replacements = { { "default:bookshelf", "default:book 3" } }
-})
-
-minetest.register_craft({
-	output = "gloopblocks:oerkki_block 2",
-	recipe = {
-		{"default:coal_lump", "default:iron_lump", "default:coal_lump"},
-		{"default:iron_lump", "default:bookshelf", "default:iron_lump"},
-		{"default:coal_lump", "default:iron_lump", "default:coal_lump"},
-	},
-	replacements = { { "default:bookshelf", "default:book 3" } }
-})
-
--- ABMs for various things
+-- ABMs for mossy cobble and mossy brick
 
 minetest.register_abm({
 	nodenames = {"default:cobble"},
@@ -524,6 +291,16 @@ minetest.register_abm({
 	chance = 20,
 	action = function(pos)
 		minetest.env:add_node (pos, {name = "default:mossycobble"})
+	end,
+})
+
+minetest.register_abm({
+	nodenames = {"default:stonebrick"},
+	neighbors = {"default:water_source", "default:water_flowing"},
+	interval = 30,
+	chance = 20,
+	action = function(pos)
+		minetest.env:add_node (pos, {name = "gloopblocks:stone_brick_mossy"})
 	end,
 })
 
@@ -554,5 +331,6 @@ gloopblocks_search_nearby_nodes = function(pos, node)
 	return false
 end
 
+dofile(minetest.get_modpath("gloopblocks").."/crafts.lua")
 
 print("Gloopblocks Loaded!")
